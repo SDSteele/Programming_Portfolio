d858394859ed553c98bcfc7a2fc116d7a1483fdd
Shannon D. Steele <110741418+SDSteele@users.noreply.github.com>
2025-09-05 21:00:58 -0400

    Create PowerShell Script.ps1

---
+++ cybersecurity_scripts_&_code/PowerShell Script.ps1
@@ -0,0 +1,18 @@
+# Lab User Cleanup - PowerShell Edition
+# Run in admin PowerShell session
+
+Write-Host "[*] Starting Windows user cleanup..."
+
+# Get all local users except Administrator
+$users = Get-LocalUser | Where-Object { $_.Name -ne "Administrator" }
+
+foreach ($u in $users) {
+    Write-Host "[+] Disabling account: $($u.Name)"
+    Disable-LocalUser -Name $u.Name
+
+    # Uncomment below to delete the account instead
+    # Write-Host "[!] Removing account: $($u.Name)"
+    # Remove-LocalUser -Name $u.Name
+}
+
+Write-Host "[*] Cleanup complete."
